b81c329e83ee987a0f3b02abb4caf805f5c84aef
mildiemilk <punthitra.dits@gmail.com>
2019-07-18 03:38:22 +0700

    Add happiness skill

--- src/components/progress.js
+++ src/components/progress.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Row, Col } from 'antd'
+import cute from '../cute.png'
 
 const progress = ({ item }) => {
   const hp = item.hp === 'None' ? 0 : item.hp
   const str = item.attacks && item.attacks.length ? item.attacks.length : 0
-  const weaknesses = item.weaknesses && item.weaknesses.length > 0 ? item.weaknesses.length : 0
+  const weak = item.weaknesses && item.weaknesses.length > 0 ? item.weaknesses.length : 0
+  let damage = 0
+  item.attacks && item.attacks.map(item => {
+    if (parseInt(item.damage, 10)) { damage = damage + parseInt(item.damage, 10) }
+  })
+  const happiness = Math.floor((hp / 10 + damage / 10 + 10 - weak / 100) / 5)
+  const cuteLevel = []
+  for (let i = 1; i <= happiness; i++) {
+    cuteLevel.push(<img className="cute-emo" src={cute} />)
+  }
   return <div className="progress">
     <Row style={{ width: '100%' }}>
       <Col span={5} style={{ fontWeight: 'bolder' }}>
@@ -32,9 +42,12 @@ const progress = ({ item }) => {
       </Col>
       <Col span={19}>
         < div className="progress-bar">
-          <span className="progress-inner" style={{ 'width': (weaknesses ? weaknesses * 100 : 0) + '%' }} />
+          <span className="progress-inner" style={{ 'width': (weak ? weak * 100 : 0) + '%' }} />
         </div>
       </Col>
+    </Row>
+    <Row style={{ width: '100%' }}>
+      {cuteLevel.map(item => item)}
     </Row>
   </div>
 }

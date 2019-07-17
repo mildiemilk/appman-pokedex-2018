77a0d4ecc79e51dddf030d12b771cf3fd2327870
mildiemilk <punthitra.dits@gmail.com>
2019-07-18 01:31:59 +0700

    Setup store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import thunkMiddleware from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import "antd/dist/antd.css"
 import './index.css'
 import App from './App'
+import rootReducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const middleware = applyMiddleware(thunkMiddleware)
+const store = createStore(rootReducer, middleware)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'))
